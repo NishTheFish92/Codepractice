048b9c1d5528116d1dcd3289c1f0c09dc70b78e1
NishTheFish92 <136304779+NishTheFish92@users.noreply.github.com>
2023-11-07 19:55:42 +0530

    Formatted the code

--- LinkedListImplimentation.cpp
+++ LinkedListImplimentation.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
 using namespace std;
 
-
-class Node {
-    public:
+class Node
+{
+public:
     int data;
-    Node* next;
+    Node *next;
 
-    Node(){
+    Node()
+    {
         data = 0;
         next = NULL;
     }
 
-    Node(int data){
+    Node(int data)
+    {
         this->data = data;
         this->next = NULL;
     }
 };
 
-class LinkedList {
-    Node* head;
-    public:
-    LinkedList(){
+class LinkedList
+{
+    Node *head;
+
+public:
+    LinkedList()
+    {
         head = NULL;
     }
     void insertNode(int);
@@ -31,24 +36,32 @@ class LinkedList {
     void deletenode(int);
 };
 
-void LinkedList::insertNode(int data){
-    Node* newnode = new Node(data);
-    if (head == NULL){
+void LinkedList::insertNode(int data)
+{
+    Node *newnode = new Node(data);
+    if (head == NULL)
+    {
         head = newnode;
         return;
     }
-    //Traversing the LL
-    Node* temp = head;
-    while (temp->next!= NULL){
+    // Traversing the LL
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
         temp = temp->next;
     }
     temp->next = newnode;
 }
 
-void LinkedList::printlist(){
-    Node* temp = head;
-    while(temp!=NULL){
-        cout<<temp->data<<" ";
+void LinkedList::printlist()
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
         temp = temp->next;
     }
+}
+
+void LinkedList::deletenode(int Nodeindex){
 }
